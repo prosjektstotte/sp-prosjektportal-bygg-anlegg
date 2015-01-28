e431fa01f5ee746b4978dfca9023676dc108d74c
Ole Kristian Mørch-Storstein <olekms@puzzlepart.com>
2015-01-28 14:18:40 +0100

    updating fields with custom task, nicer empty fasesjekkliste

--- Spiterstulen.Sherpa/DeleteDefaultDocuments/Task.cs
+++ Spiterstulen.Sherpa/DeleteDefaultDocuments/Task.cs
@@ -21,7 +21,33 @@ namespace DeleteDefaultDocuments
             {
                 items[i].DeleteObject();
             }
+            context.Load(web.Fields);
             context.ExecuteQuery();
+
+            var budget = web.Fields.GetByInternalNameOrTitle("GtStatusBudget");
+            var time = web.Fields.GetByInternalNameOrTitle("GtStatusTime");
+            var risk = web.Fields.GetByInternalNameOrTitle("GtStatusRisk");
+            context.Load(budget);
+            context.Load(time);
+            context.Load(risk);
+            context.ExecuteQuery();
+            var statusBudget = context.CastTo<FieldChoice>(budget);
+            var statusRisk = context.CastTo<FieldChoice>(risk);
+            var statusTime = context.CastTo<FieldChoice>(time);
+            
+            statusBudget.EditFormat = ChoiceFormatType.Dropdown;
+            statusBudget.UpdateAndPushChanges(true);
+            
+            statusRisk.EditFormat = ChoiceFormatType.Dropdown;
+            statusRisk.UpdateAndPushChanges(true);
+            
+            statusTime.EditFormat = ChoiceFormatType.Dropdown;
+            statusTime.Choices = new[]
+            {"Foran plan", "På plan", "Mindre forsinkelse / Forsinkelse som kan taes igjen", "Etter plan"};
+            statusTime.DefaultValue = "";
+            statusTime.UpdateAndPushChanges(true);
+            context.ExecuteQuery();
+
         }
     }
 }
