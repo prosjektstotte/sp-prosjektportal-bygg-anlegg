7e123935d94fc820c6a4c34be381440d18fa5459
tarjeieo <ormestoyl@outlook.com>
2016-06-06 15:34:34 +0200

    Removed files that should use pp default

--- Spiterstulen.Sherpa/content/SiteAssets/js/customsteps.js
+++
@@ -1,7 +0,0 @@
-﻿(function () {
-    // ceremony 
-    var deferred = GT.jQuery.Deferred();
-    GT.Project.Setup.CustomStep.promise = deferred.promise();
-    deferred.resolve("custom step - all done");
-
-})();
